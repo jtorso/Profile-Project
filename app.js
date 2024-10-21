0fb8e0c657f1bdb8325049cb86042f54ceeeeb24
user <user@shars-Air.lehman.edu>
2024-10-21 12:19:40 -0700

    Profile-Project

---
+++ app.js
@@ -0,0 +1,12 @@
+let midterm = 74;
+let hwAvg = 75;
+let projAvg = 80;
+let finalExam = 99;
+let avg1 = midterm * 0.2
+let avg2 = hwAvg * 0.15
+let avg3 = projAvg * 0.4
+let avg4 = finalExam * 0.25
+
+let finalGrade = avg1 + avg2 + avg3 + avg4
+console.log(finalGrade)
+
